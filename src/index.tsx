f293be2a2c0c1d94e0b13245828aa9f6e7d86c07
taymuronur83 <taymuronur83@gmail.com>
2026-02-19 06:25:35 +0100

    index.tsx erstellen

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import { registerRoot, Composition } from 'remotion';
+import React from 'react';
+
+// Video Bileşeni
+const MyVideo: React.FC = () => {
+  return (
+    <div style={{ 
+      flex: 1, 
+      backgroundColor: 'black', 
+      color: 'white', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      fontSize: '60px',
+      fontFamily: 'sans-serif'
+    }}>
+      Render Basarili!
+    </div>
+  );
+};
+
+// Kayıt İşlemi
+registerRoot(() => {
+  return (
+    <Composition
+      id="MyVideo"
+      component={MyVideo}
+      durationInFrames={60}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+  );
+});
