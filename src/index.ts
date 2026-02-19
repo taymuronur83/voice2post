af73f08be3abe05cb9ae5fbdde0509051e61a4f6
taymuronur83 <taymuronur83@gmail.com>
2026-02-19 03:48:40 +0100

    index.ts aktualisieren

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import { registerRoot } from 'remotion';
+import { MyVideo } from '../Components/MyVideo'; // Bileşenini buradan çağırıyoruz
 
+// Remotion projesinin ana kökü
+registerRoot(() => {
+  return (
+    /* Buradaki 'Main', render komutundaki isimle aynı olmalı.
+       Eğer hata alırsan 'MyVideo' olarak da değiştirebiliriz.
+    */
+    <MyVideo /> 
+  );
+});
